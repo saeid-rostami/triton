ed375d1e94932c537e4611ff5fd0f04f16754b07
root <saeidrostami.github@gmail.com>
2026-04-06 15:28:03 +0000

    Navi3 dropped

--- python/test/unit/language/test_conversions.py
+++ python/test/unit/language/test_conversions.py
@@ -286,7 +286,7 @@ def test_typeconvert_upcast(src_dtype, dst_dtype, device):
             return
     elif is_hip():
         if src_dtype in FP8_DTYPES and is_hip_rdna3():
-            pytest.skip(f"{src_dtype} is not supported on AMDGPU RDNA3 (Navi3)")
+            pytest.skip(f"{src_dtype} is not supported on AMDGPU RDNA3")
         if  (src_dtype == 'float8e4nv' and not (is_hip_cdna3() or is_hip_cdna4())):
             pytest.skip(f"upcasting {src_dtype} to {dst_dtype} not supported in this architecture")
         if  src_dtype == 'float8e4b15':
@@ -348,7 +348,7 @@ def test_typeconvert_downcast(src_dtype, dst_dtype, rounding, max_repr, device):
 
     if is_hip():
         if dst_dtype in FP8_DTYPES and is_hip_rdna3():
-            pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3 (Navi3)")
+            pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3")
         if dst_dtype in ('float8e4b8', 'float8e5b16') and (is_hip_cdna2() or is_hip_rdna4()):
             pytest.skip(f"{dst_dtype} is not supported on AMDGPU CDNA2 and RDNA4")
 
@@ -380,7 +380,7 @@ def test_typeconvert_downcast_clamping(src_dtype, dst_dtype, mode, device, round
             pytest.skip(f"{dst_dtype} downcast with RTNE rounding tests only supported on NVGPU with compute capability 9.0+")
 
     if dst_dtype in FP8_DTYPES and is_hip_rdna3():
-        pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3 (Navi3)")
+        pytest.skip(f"{dst_dtype} is not supported on AMDGPU RDNA3")
 
     if mode in ('inf', '-inf') and is_hip_rdna4():
         pytest.skip(f"clamping from `{mode}` is not supported on AMDGPU GFX12")
